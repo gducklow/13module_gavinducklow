34a99d4e8759819b11837a00290f16b562663c42
gducklow <gducklow@gmail.com>
2024-02-26 17:20:08 -0600

    final push before uploading

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,6 +7,13 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    
   },
   {
     sequelize,
